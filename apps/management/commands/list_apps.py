8b59d3049f05f9b755e36dae6839c3252b5093ce
Conrad Huang <conrad@cgl.ucsf.edu>
2018-10-22 17:38:06 -0700

    Add command to list all apps

---
+++ apps/management/commands/list_apps.py
@@ -0,0 +1,22 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from utils import fix_line_ending
+
+class Command(BaseCommand):
+
+    args = ""
+    help = "list all bundles"
+
+    @fix_line_ending
+    def handle(self, *args, **options):
+        self.list_all_bundles()
+
+    def list_all_bundles(self, ):
+        from apps.models import App
+        for app in App.objects.all():
+            self._list_bundle(app)
+
+    def _list_bundle(self, app):
+        print >> self.stdout, app.name, app.fullname, "active:", app.active
