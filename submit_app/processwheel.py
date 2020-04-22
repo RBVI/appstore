bfec474a4ec8ce059b9c7057af6adb40f7018533
ChimeraX Toolshed <cxtoolshed@cgl.ucsf.edu>
2020-04-22 10:30:09 -0700

    Use packaging package for dependencies parsing instead of homebrew.

--- submit_app/processwheel.py
+++ submit_app/processwheel.py
@@ -1,13 +1,18 @@
+#
+# vi: set shiftwidth=4 expandtab:
 from zipfile import BadZipfile
-from ..apps.models import App, Release, VersionRE
+from ..apps.models import App, Release
 from django.utils.encoding import smart_text
 from ..util.view_util import get_object_or_none
 from ..util.chimerax_util import Bundle
+from packaging.version import Version
+from packaging.requirements import Requirement, InvalidRequirement
 
 import logging
 logger = logging.getLogger(__name__)
 # "logger" messages land in cxtoolshed.log, e.g., logger.warning(msg)
 # Default logging level is WARNING for submit_apps.processwheel (see settings.py)
+
 
 def process_wheel(filename, expect_app_name):
     # Make sure it is a wheel and supports a single platform
@@ -53,6 +58,7 @@ def process_wheel(filename, expect_app_name):
     bundle.app_dependencies = app_dependencies
     return bundle
 
+
 def release_dependencies(app_dependencies):
     releases = []
     for dependency in app_dependencies:
@@ -62,6 +68,7 @@ def release_dependencies(app_dependencies):
         if r:
             releases.append(r)
     return releases
+
 
 def _find_release(app_name, app_version):
     # pip likes '-', but we use '_'
@@ -74,16 +81,18 @@ def _find_release(app_name, app_version):
     if not app:
         raise ValueError("missing dependency \"%s\": bundle not on toolshed" %
                          _toolshed_display_name(app_name))
-    comparison, preferred_version = _dependency_version(app_version)
+    requirement = _dependency_requirement(app_name, app_version)
     known_releases = {}
     for r in Release.objects.filter(app=app, active=True):
         known_releases[r.version] = r
-    release = _version_match(comparison, preferred_version, known_releases)
+    release = _version_match(requirement, known_releases, app_name)
     if not release:
-        raise ValueError("missing dependency on \"%s\" with version \"%s\": "
-                         "release not on toolshed" % (
-                         _toolshed_display_name(app_name), app_version))
+        raise ValueError(
+            "missing dependency on \"%s\" with version \"%s\": "
+            "release not on toolshed" % (
+                _toolshed_display_name(app_name), app_version))
     return release
+
 
 def _toolshed_display_name(app_name):
     bundle_name = app_name.replace('_', '-')
@@ -92,7 +101,9 @@ def _toolshed_display_name(app_name):
     else:
         return bundle_name
 
+
 def sort_bundles_by_dependencies(bundles):
+    # (Used by apps/devel.py)
     # 1. Prime a cache with all the bundles we are about to install.
     # 2. Make sure no bundle is missing any dependencies.
     # 3. Build dependency tree and find roots.
@@ -107,7 +118,7 @@ def sort_bundles_by_dependencies(bundles):
         try:
             d = release_cache[bundle.package]
         except KeyError:
-            release_cache[bundle.package] = {bundle.version:bundle}
+            release_cache[bundle.package] = {bundle.version: bundle}
         else:
             d[bundle.version] = bundle
         logger.debug(" prime cache: %s %s" % (bundle.package, bundle.version))
@@ -130,8 +141,8 @@ def sort_bundles_by_dependencies(bundles):
             else:
                 for v in known_releases.keys():
                     logger.debug("  known: %s" % v)
-                comparison, preferred_version = _dependency_version(app_version)
-                r = _version_match(comparison, preferred_version, known_releases)
+                requirement = _dependency_requirement(app_name, app_version)
+                r = _version_match(requirement, known_releases, app_name)
                 if r:
                     if isinstance(r, Bundle):
                         # One of the primed bundles
@@ -146,7 +157,7 @@ def sort_bundles_by_dependencies(bundles):
             try:
                 d = release_cache[r.app.fullname]
             except KeyError:
-                release_cache[r.app.fullname] = {r.version:bundle}
+                release_cache[r.app.fullname] = {r.version: bundle}
             else:
                 d[r.version] = bundle
             logger.debug(" augment cache: %s %s" % (r.app.fullname, r.version))
@@ -174,98 +185,33 @@ def sort_bundles_by_dependencies(bundles):
                 del depends_on[db]
     return ordered_bundles
 
-# Parser for version specification in bundle dependencies.
-# Currently, we only support "(>= version)" where "version"
-# is compatible with the toolshed version scheme.  Standard
-# version numbers like "0.1.4" work fine.
-import re
-_DependencyVersionRE = re.compile(r'\s*\(\s*([=!<>~]*)\s*(\S+)\)')
-_VersionRE = re.compile(r'^(\d+)(?:\.(\d+))?(?:\.(\d+|\*))?(?:\.([\w-]+))?$')
 
-def _dependency_version(s):
-    m = _DependencyVersionRE.match(s)
-    if not m:
-        raise ValueError("Unsupported dependency version format: %\"%s\"" % s)
-    (comparison, version) = m.groups()
-    return comparison, _version_tuple(version)
+def _dependency_requirement(name, specifiers):
+    try:
+        req = Requirement(f"{name}{specifiers}")
+    except InvalidRequirement as err:
+        raise ValueError(f"Invalid dependency for {name}: {err}")
+    return req
 
-def _version_tuple(v):
-    m = _VersionRE.match(v)
-    if not m:
-        raise ValueError("Unsupported version format: \"%s\"" % v)
-    (major, minor, patch, tag) = m.groups()
-    major = int(major) if major else 0
-    minor = int(minor) if minor else 0
-    patch = patch.lower() if patch else ""
-    tag = tag.lower() if tag else ""
-    return (major, minor, patch, tag)
 
-def _version_match(comparison, preferred_version, known_releases):
-    if comparison not in [">=", "=="]:
-        raise ValueError("unsupport version operator: \"%s\"" % comparison)
+def _version_match(requirement: Requirement, known_releases: {str: Release}, name: str):
     release = None
     version = None
     for v, r in known_releases.items():
         try:
-            rv = _version_tuple(v)
-        except ValueError as e:
-            raise ValueError("Unsupported version format: \"%s\" (\"%s\")" %
-                             (v, str(r)))
-        c = _version_compare(comparison, preferred_version, rv)
-        if c == 0:
-            release = r
-            version = rv
-            break
-        elif c > 0:
-            if version is None or _version_compare(">=", version, rv) > 0:
+            rv = Version(v)
+        except ValueError:
+            raise ValueError("Unsupported version format: \"%s\" (\"%s\") for name" %
+                             (v, str(r), name))
+        if rv in requirement.specifier:
+            if release is None or rv > version:
                 release = r
                 version = rv
     return release
 
-def _version_compare(comparison, preferred, rv):
-    # Assume both versions are 4-tuples.
-    # First two elements of each tuple must be integers.
-    # Third element of "preferred" is a string representing one of (integer, '', '*').
-    # Third element of "rv" is a string representing one of (integer, '').
-    # Fourth element may be one of ('', string).
-    # Return value is:
-    #  -1 if preferred > rv (rv does not satisfy requirement),
-    #   0 for an exact match, and
-    #  +1 if rv > preferred or preferred has '*' and matched rv.
 
-    newer = 1 if comparison == ">=" else -1
-
-    # Check element 0
-    if preferred[0] > rv[0]:
-        return -1
-    if preferred[0] < rv[0]:
-        return newer
-
-    # Check element 1 (element 0 must be equal)
-    if preferred[1] > rv[1]:
-        return -1
-    if preferred[1] < rv[1]:
-        return newer
-
-    # Check element 2 (element 0 and 1 must be equal)
-    if preferred[2] == '*':
-        return 1
-    try:
-        p = int(preferred[2])
-    except ValueError:
-        p = 0
-    try:
-        r = int(rv[2])
-    except ValueError:
-        r = 0
-    if p > r:
-        return -1
-    if r > p:
-        return newer
-
-    # Check element 3 (element 0, 1 and 2 must be equal)
-    if preferred[3] > rv[3]:
-        return -1
-    if preferred[3] < rv[3]:
-        return newer
-    return 0
+def _version_satisfies(requirement: Requirement, rv: Version):
+    for s in requirement.specifiers:
+        if rv not in s:
+            return False
+    return True
