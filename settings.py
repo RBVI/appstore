329ae3d0cd092a90a22b576b2763993ecf5cf37f
ChimeraX Toolshed <cxtoolshed@cgl.ucsf.edu>
2022-03-23 23:41:04 -0700

    By default, DEBUG should be False to not leak mysql password in debug output.

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ except:
 #jinja_env = Environment(extensions=['jinja2.ext.loopcontrols'])
 # Django settings for CyAppStore project.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEBUG = True 
+DEBUG = False 
 TEMPLATE_DEBUG = DEBUG
 DJANGO_STATIC_AND_MEDIA = DEBUG
 #REVIEW_ALLOW_ANONYMOUS= True
